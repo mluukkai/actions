3ca382a337a753e12c0389e8c3f8576a57fb14bc
Matti Luukkainen <mluukkai@iki.fi>
2019-12-28 18:50:08 +0200

    fix

--- tests.test.js
+++ tests.test.js
@@ -1,7 +1,7 @@
 const sum = require('./index').sum
 
 test('palindrome of a', () => {
-  const result = sum(1,2)
+  const result = sum(1, 2)
 
-  expect(result).toBe(3)
+  expect(result).toBe(1)
 })
